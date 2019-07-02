d507a37b9c79fc78c8749bd0cb6d04e37d80c0b2
Amir Mohammad Hl <dev.imorate@gmail.com>
2019-07-03 00:35:18 +0430

    assets and favicon files added

---
+++ src/main/resources/static/assets/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.badge-success').tooltip({
+        'title': "Powered by Java Spring <i class=\"fab fa-java fa-lg\"></i>",
+        'html': true,
+        'placement': 'top'
+    });
+});
